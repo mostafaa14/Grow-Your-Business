179347ef2bb5a01e587c32d72987c588feb0eb4e
mostafaa14 <ma0639620@gmail.com>
2025-11-21 21:13:36 +0200

    nino

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../../public/assets/logo.jpg";
+import logo from "../../assets/logo.jpg";
 import { IoClose } from "react-icons/io5";
 import { RiMenu2Line } from "react-icons/ri";
 
